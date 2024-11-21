a88a9ea69da999ac0a270156441f08680f453b34
Malcolm <mstone@rollins.edu>
2024-11-21 18:24:43 -0500

    Animal the animal template

--- src/Animal.java
+++ src/Animal.java
@@ -1,3 +1,56 @@
 public abstract class Animal {
+    private String name;
+    String fullPath;
+    boolean[] flags = new boolean[11];
 
+   String gender;
+   int age;
+
+   Animal () {
+
+   }   
+
+    Animal(String name) {
+         this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean[] getFlags() {
+        return flags;
+    }
+
+    public void setFlags(boolean[] flags) {
+        this.flags = flags;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFullPath() {
+        return fullPath;
+    }
+
+    public void setFullPath(String fullPath) {
+        this.fullPath = fullPath;
+    }
 }
