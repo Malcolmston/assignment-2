b773aa756f5d5a62a46d23d72906194bc508c8cb
Malcolm <mstone@rollins.edu>
2024-11-21 18:00:17 -0500

    template of Account

--- src/Account.java
+++ src/Account.java
@@ -1,3 +1,41 @@
 public class Account implements UserInfo {
+    private String name;
+    private String username;
+    private Animal[] pets;
+
+    Account () {}
+
+    Account(String name, String username) {
+        this.name = name;
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Animal[] getPets() {
+        return pets;
+    }
+
+    public void setPets(Animal[] pets) {
+        this.pets = pets;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void addPet(Animal pet) {}
+
+    public Animal getPet(int index) {}
 
 }
