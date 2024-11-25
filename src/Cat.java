71a72a3baa35857d8656c3efdda661b26ffc6295
kava117 <auraelya@live.com>
2024-11-25 16:38:25 -0500

    Update Cat.java
    added class variable, constructor, and getter

--- src/Cat.java
+++ src/Cat.java
@@ -1,3 +1,13 @@
 public class Cat extends Animal {
+  private static final String species = "Cat";
 
+  public Cat (String name, String gender, int age) {
+    super(name, gender, age);
+  }
+
+  // getters
+  public String getSpecies() {
+    return this.species;
+  }
+  
 }
