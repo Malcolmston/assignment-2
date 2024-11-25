8ca14de347210aa114952688cd871093191764f1
alexrynott <165344249+alexrynott@users.noreply.github.com>
2024-11-25 17:25:26 -0500

    added all toStrings to animal

--- src/Animal.java
+++ src/Animal.java
@@ -56,6 +56,38 @@ public abstract class Animal {
 	public String adventurous() {
 		return this.name + "likes going on advetures.";
 	}
+	public String baths() {
+		return this.name + "likes taking baths.";
+	}
+	public String cars() {
+		return this.name + "likes going on car rides.";
+	}
+	public String cosplay() {
+		return this.name + "likes to cosplay.";
+	}
+	public String eating() {
+		return this.name + "likes eating.";
+	}
+	public String energy() {
+		return this.name + "has lots of energy.";
+	}
+	public String friendly() {
+    		return this.name + "is very friendly.";
+    	}
+ 	public String running() {
+    		return this.name + "loves to run.";
+    	}
+    	public String sleeping() {
+    		return this.name + "loves to sleep.";
+    	}
+    	public String petting() {
+    		return this.name + "loves to be pet.";
+    	}
+    	public String playing() {
+    		return this.name + "loves to play.";
+    	}
+    	public String walks() {
+    		return this.name + "loves to go on walks.";
 	
 	
 }
