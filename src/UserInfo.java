3fd21c01fa807db75bc9cdf5dc28972b731f0cdc
kava117 <auraelya@live.com>
2024-11-25 17:44:51 -0500

    Update UserInfo.java
    added header comment

--- src/UserInfo.java
+++ src/UserInfo.java
@@ -1,3 +1,12 @@
+//*
+* CMS270 Group Project
+* Version 1
+* Jarod Fuentes, Ava Kopchak, Alex Rynott, Malcolm Stone
+*
+* UserInfo interface
+* Purpose: To share methods between Account and Animal class
+*/
+
 public interface UserInfo {
     public void changeName(String newName);
     public void makeProfile();
