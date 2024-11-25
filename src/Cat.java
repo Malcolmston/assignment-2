a2b3b075eded18e229bf2c7913470813432c217a
alexrynott <165344249+alexrynott@users.noreply.github.com>
2024-11-25 17:47:55 -0500

    Added toStrings to Cat

--- src/Cat.java
+++ src/Cat.java
@@ -6,8 +6,21 @@ public class Cat extends Animal {
   }
 
   // getters
+    public String playing() {
+    	return super.getName() + "loves to scratch up old boxes.";
+    }
+    public String baths() {
+		  return super.getName() + "HATES taking baths.";
+	}
+    public String eating() {
+		  return super.getName() + "loves eating more than anything in the world.";
+	}
+    public String sleeping() {
+		  return super.getName() + "likes to nap around the house all day.";
+	}
   public String getSpecies() {
     return this.species;
   }
   
 }
+
