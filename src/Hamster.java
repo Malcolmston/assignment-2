171f3c2dd21eedb5c1e06f461c7eca59be4e8958
jarodf1 <106929017+jarodf1@users.noreply.github.com>
2024-11-25 17:50:18 -0500

    Update Hamster.java

--- src/Hamster.java
+++ src/Hamster.java
@@ -1,3 +1,21 @@
-public class Hanster extends Animal {
+public class Hamster extends Animal {
+	
+	private final String species = "Hamster";
+	
+	public Hamster (String name, String gender, int age){
+		super(name,gender,age);
+	}
 
+	public String getSpecies() {
+		return this.species;
+	}
+	
+	public String running() {
+		return super.getName() + " loves running on the hamster wheel.";
+	}
+	
+	public String adventurous() {
+		return super.getName() + " loves exploring places in the home.";
+	}
+	
 }
