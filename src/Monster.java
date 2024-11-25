858a0a721afe442ca6f44940925f964c92641465
jarodf1 <106929017+jarodf1@users.noreply.github.com>
2024-11-25 18:01:48 -0500

    Update Monster.java

--- src/Monster.java
+++ src/Monster.java
@@ -1,3 +1,25 @@
-public class Monster extends Animal {
-
+public class Monster extends Animal{
+	private final String species = "Monster";
+	
+	public Monster (String name, String gender, int age) {
+		super(name,gender,age);
+	}
+	
+	public String getSpecies() {
+		return this.species;
+	}
+	
+	public String adventurous() {
+		return super.getName() + " loves exploring abandoned places.";
+	}
+	
+	public String running() {
+		return super.getName() + " loves chasing after people";
+	}
+	
+	public String playing() {
+		return super.getName() + " loves to play games like Hide-in-Seek.";
+	}
+	
+	
 }
