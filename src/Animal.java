1be301b7dc76d7c202e06cb0ee08bd51f9abacc5
Malcolm <mstone@rollins.edu>
2024-11-26 21:45:55 -0500

    updates Animal the flags list holds 12 total flags 0-11 This also will show the prper message given a flag for an event. flag[0] maps to adventurous and flags[11] maps to walks

--- src/Animal.java
+++ src/Animal.java
@@ -1,37 +1,37 @@
 /**
-* CMS270 Group Project
-* Version 1
-* Jarod Fuentes, Ava Kopchak, Alex Rynott, Malcolm Stone
-*
-* Animal abstract class
-* Purpose: To create a framework for creating Animal objects. The Animal type will be used
-* to store different pets in an array together.
-*/
+ * CMS270 Group Project
+ * Version 1
+ * Jarod Fuentes, Ava Kopchak, Alex Rynott, Malcolm Stone
+ *
+ * Animal abstract class
+ * Purpose: To create a framework for creating Animal objects. The Animal type
+ * will be used
+ * to store different pets in an array together.
+ */
 
-public abstract class Animal {
-	//Data Members
+public abstract class Animal implements UserInfo {
+	// Data Members
 	private String name;
 	private String fullPath;
-	private boolean[] flags = new boolean[11];
+	private boolean[] flags = new boolean[12];
 
 	String gender;
 	int age;
-	
+
 	// Constructor
 
-	Animal(String name, String gender, int age, boolean[] flags)  {
+	Animal(String name, String gender, int age, boolean[] flags) {
 		this.name = name;
 		this.gender = gender;
 		this.age = age;
 		this.flags = flags;
 	}
 
-	
 	// Getters and Setters
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -67,54 +67,66 @@ public abstract class Animal {
 	public void setFullPath(String fullPath) {
 		this.fullPath = fullPath;
 	}
-	
+
 	// toString Methods
+
 	public String adventurous() {
-		return this.name + " likes going on advetures.";
+		return this.name + " " + (this.flags[0] ? "likes" : "dose not like") + " going on advetures.";
 	}
 
 	public String baths() {
-		return this.name + " likes taking baths.";
+		return this.name + " " + (this.flags[1] ? "likes" : "dose not like") + " taking baths.";
 	}
 
 	public String cars() {
-		return this.name + " likes going on car rides.";
+		return this.name + " " +(this.flags[2] ? "likes" : "dose not like") + " going on car rides.";
 	}
 
 	public String cosplay() {
-		return this.name + " likes to cosplay.";
+		return this.name + " " + (this.flags[3] ? "likes" : "dose not like") + " to cosplay.";
 	}
 
 	public String eating() {
-		return this.name + " likes eating.";
+		return this.name + " " + (this.flags[4] ? "likes" : "dose not like") + " eating.";
 	}
 
 	public String energy() {
-		return this.name + " has lots of energy.";
+		return this.name + " " + (this.flags[5] ? "has" : "dose not have") + " lots of energy.";
 	}
 
 	public String friendly() {
-		return this.name + " is very friendly.";
+		return this.name + " is" + (this.flags[6] ? " " : " not ") + "very friendly.";
 	}
 
 	public String running() {
-		return this.name + " likes to run.";
+		return this.name + " " +(this.flags[7] ? "likes" : "dose not like") + " to run.";
 	}
 
 	public String sleeping() {
-		return this.name + " likes to sleep.";
+		return this.name + " " + (this.flags[8] ? "likes" : "dose not like") + " to sleep.";
 	}
 
 	public String petting() {
-		return this.name + " likes to be pet.";
+		return this.name + " " + (this.flags[9] ? "likes" : "dose not like") + " to be pet.";
 	}
 
 	public String playing() {
-		return this.name + " likes to play.";
+		return this.name + " " + (this.flags[10] ? "likes" : "dose not like") + " to play.";
 	}
 
 	public String walks() {
-    		return this.name + " likes to go on walks.";
+		return this.name + " " + (this.flags[11] ? "likes" : "dose not like") + " to go on walks.";
 	}
-	
+
+	@Override
+	public void changeName(String newName) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'changeName'");
+	}
+
+	@Override
+	public void makeProfile() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'makeProfile'");
+	}
 }
