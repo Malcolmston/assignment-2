7d4024668abb60fb47f02a7f2ef17350b1610520
Malcolm <mstone@rollins.edu>
2024-12-03 18:25:29 -0500

    show all pets

--- src/JAllPets.java
+++ src/JAllPets.java
@@ -1,3 +1,18 @@
-public class JAllPets {
 
+import java.awt.GridLayout;
+import java.util.ArrayList;
+import javax.swing.JPanel;
+
+public class JAllPets extends JPanel {
+    ArrayList<Animal> animals;
+
+    JAllPets (ArrayList<Animal> a) {
+        this.animals = a;
+
+        setLayout(new GridLayout(a.size(), 1));
+
+        animals.forEach(pet -> {
+            this.add(new JPet(pet));
+        });
+    }
 }
