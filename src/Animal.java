f9f63b8ca31bb71df5db8e193a37703da92e5c5d
Malcolm <mstone@rollins.edu>
2024-12-02 21:23:26 -0500

    fixed computeCompatibility i added a method called compareOnePet witch simplifies the code down calculating the point difference between 2 pets as opposed to the whole list of pets. This makes computeCompatibility sample as well, making the need for nested loops not nessisary. This code dose give a list of n items where n is the length of animals. This array DOSE NOT RETURN in value order

--- src/Animal.java
+++ src/Animal.java
@@ -1,12 +1,15 @@
 
+import java.util.ArrayList;
 import javax.swing.JPanel;
 
- /**
+/**
  * CMS270 Group Project
+ * 
  * @version 1.0.0
  * @authors Jarod Fuentes, Ava Kopchak, Alex Rynott, Malcolm Stone
  * 
- * @purpose To create a framework for creating Animal objects. The Animal type will be used to store different pets in an array together.
+ * @purpose To create a framework for creating Animal objects. The Animal type
+ *          will be used to store different pets in an array together.
  */
 
 public abstract class Animal implements UserInfo {
@@ -68,63 +71,89 @@ public abstract class Animal implements UserInfo {
 		this.fullPath = fullPath;
 	}
 
-	//Methods
-	/**
-	 * Calculate compatability of two Animals by comparing their flags
-	 * (also comparing age and gender, if the user wishes)
-	 * @param userPet the user's pet
-	 * @param animals complete list of animals in the database
-	 * @param discriminateAge whether the user wishes to compare ages
-	 * @param discriminateGender whether the user wishes to compare gender
-	 * @return the number of points scored for compatibility
-	 */
-	public static ArrayList<Integer> computeCompatibility(Animal userPet, ArrayList<Animal> animals, boolean discriminateAge, boolean discriminateGender) {
-		int points;
-		for(int i = 0; i < animals.size(); i++) {
-			Animal compare = animals.get(i);
-			points = compareFlags(userPet, compare);
-			if (userPet.getType() == compare.getType()) {
-				points += 2;
-			}	
-			if (discriminateAge) {
-				int addPoints = 10;
-				int ageDiff = userPet.getAge() - compare.getAge();
-				if (ageDiff < 0) {
-					addPoints += ageDiff;
-				}
-				else {
-					addPoints -= ageDiff;
-				}
-				points += addPoints;
-			}
-			if (discriminateGender) {
-				if(userPet.getGender() == compare.getGender()) {
-					points += 5;
-				}	
-			}	
-		}	
-			
-	}
-	
+	// Methods
+
 	/**
 	 * Compare the flag list between two pets
-	 * @param userPet the user's pet
+	 * 
+	 * @param userPet    the user's pet
 	 * @param comparePet the pet userPet is being compared against
 	 * @return the number of points scored for base flag compatibility
 	 */
-	public int compareFlags(Animal userPet, Animal comparePet) {
+	private  static int compareFlags(Animal userPet, Animal comparePet) {
 		int points = 0;
-		for(int i = 0; i < userPet.getFlags().length(); i++) {
-			int[] userFlags = userPet.getFlags();
-			int[] compareFlags = comparePet.getFlags();
+		for (int i = 0; i < userPet.getFlags().length; i++) {
+			boolean[] userFlags = userPet.getFlags();
+			boolean[] compareFlags = comparePet.getFlags();
 
-			if(userFlags[i] == compareFlags[i]) {
+			if (userFlags[i] == compareFlags[i]) {
 				points += 1;
-		}	
+			}
+		}
+		return points;
+
+	}
+
+	/**
+	 * Compare the flag list between two pets
+	 * 
+	 * @param a                  the first pet
+	 * @param b                  the second pet
+	 * @param discriminateAge    whether the user wishes to compare ages
+	 * @param discriminateGender whether the user wishes to compare gender
+	 * @return the number of points scored for base flag, age, type, etc...
+	 *         compatibility
+	 */
+	private static int compareOnePet(Animal a, Animal b, boolean discriminateAge, boolean discriminateGender) {
+		int points = 0;
+
+		// add flag points
+		points += compareFlags(a, b);
+
+		// add age points if both pets are of the same type
+		if (a.getClass() == b.getClass()) {
+			points += 2;
+		}
+
+		// get the difference in age and add points based on that
+		if (discriminateAge) {
+			int addPoints = 10;
+			int ageDiff = a.getAge() - b.getAge();
+			if (ageDiff < 0) {
+				addPoints += ageDiff;
+			} else {
+				addPoints -= ageDiff;
+			}
+			points += addPoints;
+		}
+
+		// get the gender difference and add points based on that
+		if (discriminateGender) {
+			if (a.getGender().equals(b.getGender())) {
+				points += 5;
+			}
+		}
+
+		return points;
+
+	}
+
+	/**
+	 * Compare the flag list between two pets
+	 * 
+	 * @param userPet    the user's pet
+	 * @param comparePet the pet userPet is being compared against
+	 * @return the number of points scored for base flag compatibility
+	 */
+	public static ArrayList<Integer> computeCompatibility(Animal userPet, boolean discriminateAge,
+			boolean discriminateGender, ArrayList<Animal> animals) {
+		ArrayList<Integer> points = new ArrayList<>();
+		for (int i = 0; i < animals.size(); i++) {
+			points.add(compareOnePet(userPet, animals.get(i), discriminateAge, discriminateGender));
+		}
 		return points;
 	}
 
-	
 	public String adventurous() {
 		return this.name + " " + (this.flags[0] ? "likes" : "does not like") + " going on advetures.";
 	}
@@ -134,7 +163,7 @@ public abstract class Animal implements UserInfo {
 	}
 
 	public String cars() {
-		return this.name + " " +(this.flags[2] ? "likes" : "does not like") + " going on car rides.";
+		return this.name + " " + (this.flags[2] ? "likes" : "does not like") + " going on car rides.";
 	}
 
 	public String cosplay() {
@@ -154,7 +183,7 @@ public abstract class Animal implements UserInfo {
 	}
 
 	public String running() {
-		return this.name + " " +(this.flags[7] ? "likes" : "does not like") + " to run.";
+		return this.name + " " + (this.flags[7] ? "likes" : "does not like") + " to run.";
 	}
 
 	public String sleeping() {
