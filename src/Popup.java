4864b6063eb75951d1cb0d10c1feb0cde6c93e38
Malcolm <mstone@rollins.edu>
2024-12-03 18:44:53 -0500

    manage the 3 popup types

--- src/Popup.java
+++ src/Popup.java
@@ -1,3 +1,57 @@
-public abstract class Popup {
 
+import java.awt.GridLayout;
+import java.util.ArrayList;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+
+public class Popup extends JDialog {
+    JButton closeBTN = new JButton("Close");
+
+    public Popup (Account account) {   
+        setTitle("User: " + account.getUsername());
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        setSize(300, 150);
+
+        JPanel m = new JUser(account);
+
+        setLayout( new GridLayout(2, 1));
+
+        add(m);
+
+        add(closeBTN);
+
+    }
+
+    public Popup (Animal animal) {
+        setTitle("Animal: " + animal.getName());
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        setSize(300, 150);
+
+        JPanel m = new JPet(animal);
+
+        setLayout( new GridLayout(2, 1));
+
+        add(m);
+
+        add(closeBTN);
+    }
+
+    public Popup (ArrayList<Animal> animals) {
+        setTitle("All Pets");
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        setSize(300, 150);
+
+        JPanel m = new JAllPets(animals);
+
+        setLayout( new GridLayout(2, 1));
+
+        add(m);
+
+        add(closeBTN);
+    }
+    
 }
