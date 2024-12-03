2a926b0f6267e5ca5bed2e37443fe78ff078fb91
kava117 <auraelya@live.com>
2024-12-02 19:07:42 -0500

    fixing malons spelling errors

--- src/Hamster.java
+++ src/Hamster.java
@@ -15,12 +15,12 @@ public class Hamster extends Animal {
 
 	@Override
 	public String adventurous() {
-		return super.getName() + " " + (this.getFlags()[0] ? "loves" : "dose not like" ) + " exploring places in the home.";
+		return super.getName() + " " + (this.getFlags()[0] ? "loves" : "does not like" ) + " exploring places in the home.";
 	}
 
 	@Override
 	public String running() {
-		return super.getName() + " " +(this.getFlags()[7] ? "loves" : "dose not like") + " running on the hamster wheel.";
+		return super.getName() + " " +(this.getFlags()[7] ? "loves" : "does not like") + " running on the hamster wheel.";
 	}
 
 	public String getUrl() {
