250df972292c17cfeeed5d6338fcaea301199b03
Malcolm <mstone@rollins.edu>
2024-12-03 09:43:50 -0500

    added data members as well as getters and setters

--- src/Popup.java
+++ src/Popup.java
@@ -1,3 +1,38 @@
-public class Popup {
+import javax.swing.JPopupMenu;
+import javax.swing.JToggleButton;
+
+public class Popup extends JPopupMenu {
+    private boolean dicAge = true;
+    private boolean dicGender = true;
+
+    private final JToggleButton button = new JToggleButton("Discriminate on age");
+    private final JToggleButton button2 = new JToggleButton("Discriminate on gender");
+
+
+    public boolean getDicAge() {
+        return dicAge;
+    };
+
+
+    public boolean isDicGender() {
+        return dicGender;
+    }
+
+    public void setDicAge(boolean dicAge) {
+        this.dicAge = dicAge;
+    }
+
+    public void setDicGender(boolean dicGender) {
+        this.dicGender = dicGender;
+    }
+
+    public JToggleButton getButton() {
+        return button;
+    }
+
+    public JToggleButton getButton2() {
+        return button2;
+    }
+
 
 }
