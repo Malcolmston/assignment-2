14d051dc3f634579212458a2e01c55d8fce78c42
Malcolm <mstone@rollins.edu>
2024-12-03 18:25:43 -0500

    show a user and there pets

--- src/JUser.java
+++ src/JUser.java
@@ -1,3 +1,36 @@
-public class JUser {
+import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.Arrays;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
+public class JUser extends JPanel {
+    private String username;
+    private int numPets;
+    private Animal[] animals;
+
+    private static ArrayList<Animal> toArrayList(Animal[] animals) {
+        ArrayList<Animal> list = new ArrayList<>();
+        list.addAll(Arrays.asList(animals));
+        return list;
+    }
+
+    JUser(Account account) {
+        super();
+
+        this.username = account.getUsername();
+        this.numPets = account.getNumPets();
+        this.animals = account.getPets();
+
+
+        setLayout(new GridLayout(3, 1));
+
+        JLabel userLabel = new JLabel("User: " + username);
+        JLabel numPetsLabel = new JLabel("Number of pets: " + numPets);
+
+        add(userLabel);
+        add(numPetsLabel);
+
+        add(new JAllPets( JUser.toArrayList( animals ) ) );
+    }
 }
