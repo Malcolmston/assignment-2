1da2e43123eef699b6e671797719d5807cc01e75
Malcolm <mstone@rollins.edu>
2024-12-02 21:03:48 -0500

    app - App java classes start with uppercase letters

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 
-public class app {
+public class App {
 public static void main(String[] args) {
         try (Scanner scanner = FileReader.getScanner()) {
 
