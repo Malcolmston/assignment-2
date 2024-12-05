789275292058c6bcf1d5dabad023e491cf22ec34
alexrynott <165344249+alexrynott@users.noreply.github.com>
2024-12-04 21:50:32 -0500

    file header

--- src/CreateListener.java
+++ src/CreateListener.java
@@ -1,3 +1,10 @@
+/**
+ * CMS270 Group Project
+ * @version 1.0.0
+ * @authors Jarod Fuentes, Ava Kopchak, Alex Rynott, Malcolm Stone
+ * 
+ * @purpose Creates a type of on click event
+ */
 public interface CreateListener {
     void onClose(Animal a);
 }
