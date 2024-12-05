d984112fd055e523b87e6ca3f64f64762eff612a
kava117 <auraelya@live.com>
2024-12-04 21:48:10 -0500

    added header comment

--- src/PopUpMgr.java
+++ src/PopUpMgr.java
@@ -1,3 +1,12 @@
+/**
+ * CMS270 Group Project
+ * @version 1.0.0
+ * @authors Jarod Fuentes, Ava Kopchak, Alex Rynott, Malcolm Stone
+ * 
+ * @purpose An interface to share a behavior between the Animal 
+ * and Account classes. Aids in the creation and use of GUI pop-ups.
+ */
+
 public interface PopUpMgr {
   public void show();
 }
