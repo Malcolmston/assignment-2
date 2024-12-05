dddb30c98bfd71391bf59c8c57db6ca4e511689c
kava117 <auraelya@live.com>
2024-12-04 21:54:37 -0500

    added header comment (UNFINISHED)

--- src/CloseListener.java
+++ src/CloseListener.java
@@ -1,3 +1,11 @@
+/**
+ * CMS270 Group Project
+ * @version 1.0.0
+ * @authors Jarod Fuentes, Ava Kopchak, Alex Rynott, Malcolm Stone
+ * 
+ * @purpose 
+ */
+
 public interface CloseListener {
     void onClose(boolean dicAge, boolean dicGender);
 }
